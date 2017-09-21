6f2a8a2c84d43f3ba9e9c9915e2da52c3908e2fa
Andre Duarte <andre.duarte@mobilityhouse.com>
2017-09-21 11:31:15 +0200

    Methods Overload

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,13 +1,34 @@
 
 class Card:
 
-    def __init__(self, suit):
+    points_mapping = {'A': 11, 'J': 10, 'Q': 10, 'K': 10}
+
+    def __init__(self, suit, value):
 
         if suit not in ['spade', 'club', 'diamond', 'heart']:
             raise ValueError(
                 "{} must be in ['spade', 'club', 'diamond', 'heart']")
 
         self.suit = suit
+
+        if value not in ['A', 'Q', 'K', 'J'] and value not in list(range(2, 11)):
+            raise ValueError
+        self.value = value
+
+    def mapping_points(self):
+        if isinstance(self.value, str):
+            return self.points_mapping[self.value]
+        else:
+            return self.value
+
+    def __add__(self, other):
+        return self.mapping_points() + other.mapping_points()
+
+    def __gt__(self, other):
+        return self.mapping_points() > other.mapping_points()
+
+    def __eq__(self, other):
+        return self.mapping_points() == other.mapping_points()
 
 
 class Deck:
