dc50652dce928a0b650d66802cc34a2c21e6e64f
Andre Duarte <andre.duarte@mobilityhouse.com>
2017-10-12 11:47:34 +0200

    Adding TCP Server and Client functionality and pbr

---
+++ blackjack/cli.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+from blackjack.server import BlackjackServer
+from blackjack.client import BlackjackClient
+
+
+def get_parser():  # pragma: no coverage
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="cmd")
+
+    server = subparsers.add_parser("serve", description="Start a blackjack game server")
+    client = subparsers.add_parser("join", description="Join a Blackjack server")
+
+    client.add_argument("server", type=str, help="The server to join")
+
+    return parser
+
+
+def main():
+    parser = get_parser()
+    options = parser.parse_args()
+    if not options.cmd:
+        parser.print_help()
+
+    if options.cmd == 'serve':
+        with BlackjackServer() as server:
+            server.serve_forever()
+
+    if options.cmd == 'join':
+        BlackjackClient(options.server).run()
